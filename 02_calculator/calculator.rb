f7eec9a3677b7885cf7f7f55935bb886fbd3beaa
Abduraimov Mo'minjon <muminjonguru@outlook.com>
2019-04-30 18:42:34 +0500

    solved

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,36 @@
 #write your code here
+def add(num1, num2)
+  num1 + num2
+end
+
+def subtract(num1, num2)
+  num1 - num2
+end
+
+def sum(arr)
+  s = 0
+  arr.each { |i| s += i }
+  s
+end
+
+def multiply(*numbers)
+  multip = 1
+  numbers.each { |i| multip *= i }
+  multip
+end
+
+def power(base, pow)
+  res = 1
+  pow.times do
+    res *= base
+  end
+  res
+end
+
+def factorial(number)
+  if number <= 1
+    1
+  else
+    number * factorial(number - 1)
+  end
+end
