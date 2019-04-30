326f5866b75993e895ff8db49ced57993a8bdb44
Abduraimov Mo'minjon <muminjonguru@outlook.com>
2019-04-30 18:43:58 +0500

    solved

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,9 @@
 #write your code here
+def translate(string)
+  string.split.map do |word|
+    word=~ /^([^aeiouyq]*(qu)?)(.*)$/x
+    first_phoneme = $1
+    rest_of_word = $3
+    "#{rest_of_word}#{first_phoneme}ay"
+  end.join(" ")
+end
