f0be29c0635f9418c49907dfd7558eecf51fef6e
Abduraimov Mo'minjon <muminjonguru@outlook.com>
2019-04-30 18:44:28 +0500

    problems solved

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,10 @@
 class Book
-# write your code here
+  attr_writer :title
+  def title
+    excepted_words = %w[in on the a over a an and or of]
+    str_arr = @title.strip.split(' ')
+    str_arr[0].capitalize!
+    str_arr.map { |i| excepted_words.include?(i) ? i : i.capitalize! }
+    @title = str_arr.join(' ')
+  end
 end
